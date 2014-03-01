a866b2a61378e002091f93a0a47019cfff4c50c6
Student <user@chromebox-001.(none)>
2014-02-28 17:14:57 -0800

    Added book and booklist

--- ex05.js
+++ ex05.js
@@ -6,5 +6,17 @@ var book = function(bookTitle, genre, author, read, readDate){
     this.readDate = readDate || Date();
 };
 
+var bookList = function(booksRead,booksNotRead,bookShelf){
+    this.booksRead = booksRead || "0";
+    this.booksNotRead = booksNotRead || "0";
+    console.log(bookShelf);
+    this.bookShelf = bookShelf || "no books on shelf";
+};
+
+
 var firstBook = new book("title1","genre1","Author1",true, "");
 console.log("The book is : " + firstBook.bookTitle + " " + firstBook.genre + " " + firstBook.author + " " + firstBook.read +" " + firstBook.readDate);
+
+var mylist = new bookList("2", "3", "[A,B]");
+console.log("The books Read and Unread are: " + mylist.booksRead + " AND "+ mylist.booksNotRead);
+console.log("The bookshelf is : " + mylist.bookShelf);
